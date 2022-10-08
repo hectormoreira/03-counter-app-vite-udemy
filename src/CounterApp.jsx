0e4df9e0e86b97acf3b4770d71369e4b45d766d9
Héctor Moreira <hector.moreirac@gmail.com>
2022-10-08 12:47:40 -0500

    end section

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
+
 export const CounterApp = ({ value }) => {
+    const [counter, setCounter] = useState(value)
+
+    const handleAdd = () => {
+        setCounter(counter + 1)
+        // setCounter((c) => c + 1)
+    }
+
+    const handleSubstract = () => setCounter(counter - 1)
+    const handleReset = () => setCounter(value)
+
     return (
         <>
             <h1>Counter</h1>
-            <h2>{value}</h2>
+            <h2> {counter} </h2>
+
+            <button onClick={handleAdd}>+1</button>
+            <button onClick={handleSubstract}>-1</button>
+            <button onClick={handleReset}>Reset</button>
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import './style.css';
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         {/* <FirstApp title="Hola soy un titulo" /> */}
-        <CounterApp value={3} />
+        <CounterApp value={5} />
     </React.StrictMode>
 )
