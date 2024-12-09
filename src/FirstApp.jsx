9deac5e5996c94dfb9a32152d70ccef8b8c35232
hectormoreira <hector.moreirac@gmail.com>
2024-12-09 17:11:32 -0500

    toMatchSnapshot

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 export const FirstApp = ({ title, subTitle }) => {
   return (
     <>
-      <h2>{title}</h2>
+      <h1>{title}</h1>
       <p>{subTitle}</p>
     </>
   );
